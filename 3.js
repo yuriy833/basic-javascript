9baafc33cd8560830da0c9a5a6ae98765ca54855
Yuriy833 <mr.prezident2018@mail.ru>
2022-05-31 20:29:31 +0300

    add answer lesson-4

--- 3.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const products = [{ id: 3, price: 200, }, { id: 4, price: 900, }, { id: 1, price: 1000, },];
-products.forEach(element => console.log(element.price - (element.price / 100 * 15)));
--- 6.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-for (let i = 'X'; i.length < 21; i += 'X') {
-    console.log(i);
-}
